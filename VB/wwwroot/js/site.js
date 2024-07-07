8b7071c254508c59043f7e1ca6ddf1fc688598a7
erasmusjdd <erasmusjdd@gmail.com>
2024-07-07 18:20:21 +0200

    removed un-needed js

--- VB/wwwroot/js/site.js
+++ VB/wwwroot/js/site.js
@@ -2,12 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-window.addEventListener("load", () => {
-    const uri = document.getElementById("qrCodeData").getAttribute('data-url');
-    new QRCode(document.getElementById("qrCode"),
-        {
-            text: uri,
-            width: 150,
-            height: 150
-        });
-});
